d71d0a4b474423636d9a89cd01944d5916bef88b
julio <julior0518@gmail.com>
2022-04-09 12:14:11 -0400

    added logo and better UI

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,18 @@
 
 import addModel from '../assets/addModel.png'
 import showRoutes from '../assets/showRoutes.png'
+import logoWhite from '../assets/logoWhite.png'
 
 function Navbar({setTab}){
 
     return (
         <div className="Navbar">
-            <img src={addModel} style={{width: "3vw"}} onClick={()=>{setTab("createModel")}}></img>
-            <img src={showRoutes} style={{width: "3vw"}} onClick={()=>{setTab("createControllers")}}></img>
-
+            <div style={{display:'flex', flexDirection:'column'}}>
+                <img src={addModel} style={{width: "3vw", margin:"1vh 0 4vh"}} onClick={()=>{setTab("createModel")}}></img>
+                <img src={showRoutes} style={{width: "3vw"}} onClick={()=>{setTab("createControllers")}}></img>
+            </div>
+            <img src={logoWhite} style={{width: "100%", borderTop: "4px solid rgb(39, 157, 108)", paddingTop:"2vh"}} onClick={()=>{setTab("createModel")}}></img>
+            
         </div>
     )
 }
