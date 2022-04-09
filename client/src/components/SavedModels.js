a31f998f558ffe05820193f14f854858ff9753ec
julio <julior0518@gmail.com>
2022-04-09 15:27:00 -0400

    added mansory layout to render of saved models

--- client/src/components/SavedModels.js
+++ client/src/components/SavedModels.js
@@ -1,27 +1,43 @@
+import Masonry from 'react-masonry-css'
+
+const breakpointObj = {
+    deafault: 4,
+    3000: 5,
+    2000: 4,
+    1200: 3,
+    1000: 2, 
+    500: 1,
+}
 
 function SavedModels ({allModels}) {
-    console.log(allModels)
+    console.log(allModels, 111)
 
     return(
-        <div className="SavedModels">
-
-            asdf
-
-            {/* <h4 className='title'>{`models/${model.modelTitle}.js`}</h4> 
-            <div className='renderCode'>
-            <p>const mongoose = require('mongoose');</p>
-            <p>const Schema = mongoose.Schema;</p>
-            <br></br>
-            <p> {`const ${title} = new Schema({ `} </p>
-            {allKeys.map((k, i)=>(
-                <div key={i}>
-                    <p>{`${k.key}: {${k.inputType}},`}</p>
+        <div className="SavedModels" >
+            <Masonry  style={{display:"flex", flexWrap:"wrap"}}breakpointCols={breakpointObj}>
+            {allModels.map((M, i)=> (
+                <div className='modelsModel model' style={{margin:"0 1% 2% 0", width:'max-content', height:"max-content"}}>
+                        <h4 className='title'>{`models/${M.modelTitle}.js`}</h4> 
+                <div className='renderCode'>
+                    <p>const mongoose = require('mongoose');</p>
+                    <p>const Schema = mongoose.Schema;</p>
+                    <br></br>
+                    <p> {`const ${M.modelTitle} = new Schema({ `} </p>
+                    {M.modelKeys.map((k, i)=>(
+                        <div key={i}>
+                            <p style={{fontWeight: "bold"}}>{`${k.key}: {${k.inputType}},`}</p>
+                        </div>
+                    ))}
+                    <p> {`},{ timestamp: true },);`}</p>
+                    <br></br>
+                    {` module.exports = ${M.modelTitle};`} 
+                    
                 </div>
+                </div>
+
             ))}
-            <br></br>
-            <p> {`},`}</p>
-            {`{ timestamp: true }); module.exports = ${title};`}  */}
-                        
+
+            </Masonry>
         </div>
     )
 }
